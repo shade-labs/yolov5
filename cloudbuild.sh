85b19ff7c6c36d9a1307a9d4ca4e378039b7c93b
Jeremy Liu <jeremylliu@gmail.com>
2022-07-14 15:30:11 -0400

    fixed shell script

--- cloudbuild.sh
+++ cloudbuild.sh
@@ -1,8 +1,7 @@
 ros_versions=( "foxy" "galactic" "humble" "rolling")
 
-for i in $(seq 1 4);
+for ROS_DISTRO in "${ros_versions[@]}"
 do
-  ROS_DISTRO="${ros_versions[i]}"
   gcloud builds submit --config cloudbuild.yaml . --substitutions=_ROS_DISTRO="$ROS_DISTRO" --timeout=86400 &
-  echo Dispached ROS "$ROS_DISTRO" using shaderobotics/pytorch:"$ROS_DISTRO" base image
+  echo Dispached ROS "$ROS_DISTRO" using shaderobotics/yolov5:"$ROS_DISTRO" base image
 done
